2adf68c9ad7b2dcbbc527134d905b318b6c51ec4
Antonio Guerrero <a.guerrero.esp94@gmail.com>
2021-06-18 11:19:12 -0500

    fix models.py

--- gestion_condominios/models.py
+++ gestion_condominios/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
-# Create your models here.
+class AdminCondominium(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Condominium(models.Model):
+    administrator_id = models.ForeignKey(AdminCondominium, on_delete=models.CASCADE)
+
+class Department(models.Model):
+    condominium_id    = models.OneToOneField(Condominium, on_delete=models.CASCADE)
+    department_number = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(9999)], blank=False)
+    department_block  = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(9999)], blank=False)
+    number_habitants  = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(4)], blank=False)
+    department_owner  = models.CharField(max_length=200, blank=False)
+
+class Department_owner(models.Model):
+    user_id              = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_regex          = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    p_number             = models.CharField(validators=[phone_regex], max_length=16, blank=False)
+    p_number_emergency   = models.CharField(validators=[phone_regex], max_length=16, blank=False)
+    department_id        = models.OneToOneField(Department, on_delete=models.CASCADE)
+    account_confirmation = models.BooleanField(default=False)
+
+class Comments(models.Model):
+    owner_id        = models.OneToOneField(Department_owner, on_delete=models.CASCADE)
+    condominium_id  = models.OneToOneField(Condominium, on_delete=models.CASCADE)
+    comment_title   = models.CharField(max_length=100, blank=False)
+    comment         = models.TextField(blank=False)
+    flaw_title      = models.CharField(max_length=100, blank=False)
+    flaw            = models.TextField(blank=False)
+
+class FinancialStatus(models.Model):
+    condominium_id = models.OneToOneField(Condominium, on_delete=models.CASCADE)
+    type_detail    = models.CharField(max_length=100, blank=False)
+    income         = models.FloatField(validators=[MinValueValidator(0)], blank=True)
+    expenses       = models.FloatField(validators=[MinValueValidator(0)], blank=True)
+    details        = models.TextField(blank=False)
