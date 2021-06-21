2f51e06fd335fd9768b815e93cf87bd96b1a1673
Antonio Guerrero <a.guerrero.esp94@gmail.com>
2021-06-21 17:40:35 -0500

    add Base model

--- gestion_condominios/models.py
+++ gestion_condominios/models.py
@@ -2,20 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
-class AdminCondominium(models.Model):
+class BaseModelCustom(models.Model):
+    created_at = models.DateTimeField(
+        'created at', 
+        auto_now_add=True,
+        help_text='Date time when the object was created.'
+    )
+
+    updated_at = models.DateTimeField(
+        'modified at', 
+        auto_now=True,
+        help_text='Date time when the object was last modified.'
+    )
+
+    class Meta:
+        """Meta option."""
+        abstract = True
+
+class AdminCondominium(BaseModelCustom, models.Model):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class Condominium(models.Model):
+class Condominium(BaseModelCustom, models.Model):
     administrator_id = models.ForeignKey(AdminCondominium, on_delete=models.CASCADE)
 
-class Department(models.Model):
+class Department(BaseModelCustom, models.Model):
     condominium_id    = models.OneToOneField(Condominium, on_delete=models.CASCADE)
     department_number = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(9999)], blank=False)
     department_block  = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(9999)], blank=False)
     number_habitants  = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(4)], blank=False)
     department_owner  = models.CharField(max_length=200, blank=False)
 
-class Department_owner(models.Model):
+class Department_owner(BaseModelCustom, models.Model):
     user_id              = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_regex          = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     p_number             = models.CharField(validators=[phone_regex], max_length=16, blank=False)
@@ -23,7 +40,7 @@ class Department_owner(models.Model):
     department_id        = models.OneToOneField(Department, on_delete=models.CASCADE)
     account_confirmation = models.BooleanField(default=False)
 
-class Comments(models.Model):
+class Comments(BaseModelCustom, models.Model):
     owner_id        = models.OneToOneField(Department_owner, on_delete=models.CASCADE)
     condominium_id  = models.OneToOneField(Condominium, on_delete=models.CASCADE)
     comment_title   = models.CharField(max_length=100, blank=False)
@@ -31,7 +48,7 @@ class Comments(models.Model):
     flaw_title      = models.CharField(max_length=100, blank=False)
     flaw            = models.TextField(blank=False)
 
-class FinancialStatus(models.Model):
+class FinancialStatus(BaseModelCustom, models.Model):
     condominium_id = models.OneToOneField(Condominium, on_delete=models.CASCADE)
     type_detail    = models.CharField(max_length=100, blank=False)
     income         = models.FloatField(validators=[MinValueValidator(0)], blank=True)
